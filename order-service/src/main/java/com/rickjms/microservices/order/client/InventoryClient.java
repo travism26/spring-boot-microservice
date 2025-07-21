2db1da6d9eb1b8088d0d4c6f4216b5142396edfd
travis martin <travismartin266@gmail.com>
2025-07-21 08:55:02 -0300

    refactor

--- order-service/src/main/java/com/rickjms/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/rickjms/microservices/order/client/InventoryClient.java
@@ -1,14 +1,11 @@
 package com.rickjms.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "${inventory.api.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity);
 
 }
---
+++ order-service/src/main/java/com/rickjms/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,25 @@
+package com.rickjms.microservices.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.rickjms.microservices.order.client.InventoryClient;
+
+public class RestClientConfig {
+
+    @Value("${inventory.api.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
